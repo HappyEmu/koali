75c18631ae12795cfe9e75ce8fcb607b6df58cc5
Urs Gerber <urs.gerber@devedis.ch>
2025-05-26 21:14:01 +0200

    Add storage

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250526_183620 from './20250526_183620';
+import * as migration_20250526_191315 from './20250526_191315';
 
 export const migrations = [
   {
     up: migration_20250526_183620.up,
     down: migration_20250526_183620.down,
-    name: '20250526_183620'
+    name: '20250526_183620',
+  },
+  {
+    up: migration_20250526_191315.up,
+    down: migration_20250526_191315.down,
+    name: '20250526_191315'
   },
 ];
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,4 +1,4 @@
-// storage-adapter-import-placeholder
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 import { vercelPostgresAdapter } from '@payloadcms/db-vercel-postgres'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
@@ -34,6 +34,14 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    vercelBlobStorage({
+      enabled: true,
+      collections: {
+        media: {
+          prefix: 'media',
+        },
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    }),
   ],
 })
