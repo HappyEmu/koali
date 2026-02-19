7d74e29e68ac3fb8c73a1ea036c7291015090659
Urs Gerber <ug.gerber@gmail.com>
2026-02-19 21:05:20 +0100

    Add nodemailer for dev env

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,6 +1,7 @@
 import { gcsStorage } from '@payloadcms/storage-gcs'
 import { vercelPostgresAdapter } from '@payloadcms/db-vercel-postgres'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
+import { nodemailerAdapter } from '@payloadcms/email-nodemailer'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
@@ -15,6 +16,17 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+  ...(process.env.NODE_ENV !== 'production' && {
+    email: nodemailerAdapter({
+      defaultFromAddress: 'noreply@koali.ch',
+      defaultFromName: 'Koali',
+      transportOptions: {
+        host: '127.0.0.1',
+        port: 1025,
+        ignoreTLS: true,
+      },
+    }),
+  }),
   admin: {
     user: Users.slug,
     importMap: {
